7c900f061bd8de3acc2afbf31d0f22a1e75af43c
Davila99 <davilaeliseo453@gmail.com>
2021-11-29 23:18:05 -0600

    Agragando la parte de navegacion

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Calificaciones from './src/screens/Calificaciones';
 
 export default function App() {
